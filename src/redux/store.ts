d5e86c013e730c314b2b8885dfaf2f5cc83ae095
manik <maniksarker652@gmail.com>
2025-11-04 12:29:45 +0600

    s

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,13 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import baseApis from "./baseApis";
 
 const store = configureStore({
-    reducer: {
-        [baseApis.reducerPath]: baseApis.reducer,
-    },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(baseApis.middleware),
+  reducer: {
+    [baseApis.reducerPath]: baseApis.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApis.middleware),
 });
-
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+//
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 export default store;
